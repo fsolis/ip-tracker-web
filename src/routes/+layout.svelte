84e4cf9b404c7a55ccc3a41ad399181ba3ad7b4d
Freddy <fsolis.se@gmail.com>
2025-01-24 19:29:26 -0800

    Fixed layout - Added input template

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,20 @@
 <script lang="ts">
   import "../app.css";
   let { children } = $props();
+
+  import desktopBg from "$lib/images/pattern-bg-desktop.png";
+  import mobileBg from "$lib/images/pattern-bg-mobile.png";
+  import { isMdScreen } from "../stores/dims.svelte";
+
+  const bgImage = $isMdScreen ? desktopBg : mobileBg;
 </script>
 
-{@render children()}
+<main class="h-full my-10 mx-10 p-0">
+  <div
+    class="absolute grid grid-rows-[30vh_1fr] h-screen bottom-0 top-0 left-0 right-0 z-0"
+  >
+    <div class="bg-cover" style="background-image: url({bgImage});"></div>
+    <div></div>
+  </div>
+  {@render children()}
+</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,18 @@
   const bgImage = $isMdScreen ? desktopBg : mobileBg;
 </script>
 
-<div class="grid grid-rows-[30vh_1fr] h-screen">
-  <div class="bg-cover" style="background-image: url({bgImage});"></div>
-  <p>Welcome to the home page!</p>
+<div class="relative flex flex-col flex-grow mx-auto">
+  <h1 class="font-bold text-3xl text-white text-center">IP Address Tracker</h1>
+
+  <div class="self-center flex flex-row justify-center max-w-lg w-full">
+    <input
+      type="text"
+      placeholder="Search for any IP address or domain"
+      class="w-full mt-5 px-5 py-3 rounded-l-lg outline-none"
+    />
+
+    <button class="mt-5 ml-0 px-5 py-3 bg-black text-white rounded-r-lg">
+      Search
+    </button>
+  </div>
 </div>
